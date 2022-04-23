dd207b3269f16218e64865965cfbe984ef2607ca
6emre <emre.derman@ug.bilkent.edu.tr>
2022-04-23 23:46:18 +0300

    NorthPole distance Added.

--- gps_application/src/App.js
+++ gps_application/src/App.js
@@ -18,7 +18,8 @@ class App extends React.Component {
         moonInfo : "",
         countryText : "",
         responseFlag : false,
-        input_option : 0
+        input_option : 0,
+        distanceNorthPole : 0,
         };
   
       this.handleChange = this.handleChange.bind(this);
@@ -26,6 +27,7 @@ class App extends React.Component {
       this.handleOptionChange = this.handleOptionChange.bind(this);
       this.gpsDidMount = this.gpsDidMount.bind(this); 
       this.showPosition = this.showPosition.bind(this); 
+      this.distance = this.distance.bind(this); 
     
     //   this.latitudeSetter = this.latitudeSetter.bind(this);
     //   this.longitudeSetter = this.longitudeSetter.bind(this);
@@ -44,13 +46,33 @@ class App extends React.Component {
         if(name == "latitude")
             this.setState({latitude: value,responseFlag: false});
     }
-   
+    distance(lat1, lon1, lat2, lon2, unit) {
+        var radlat1 = Math.PI * lat1/180
+        var radlat2 = Math.PI * lat2/180
+        var radlon1 = Math.PI * lon1/180
+        var radlon2 = Math.PI * lon2/180
+        var theta = lon1-lon2
+        var radtheta = Math.PI * theta/180
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        dist = Math.acos(dist)
+        dist = dist * 180/Math.PI
+        dist = dist * 60 * 1.1515
+        if (unit=="K") { dist = dist * 1.609344 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        return dist
+    }
+
     async handleSubmit(event) {
+        Geocode.setApiKey("AIzaSyAvs-JNQhhyMi38iNEmRe45ehldcnSNce8");
+        Geocode.setLanguage("en");
+        Geocode.setLocationType("ROOFTOP");
         const moon = createMoon();
         const distance = await moon.getDistanceToEarth();
         const delta = await moon.getAngularDiameter();
         var _moonInfo = 'distance is : ' + distance + "\n"+ 'delta is : ' + delta;
         var _countryText;
+       var _distanceNorthPole = this.distance(this.state.latitude,this.state.longitude,90,0);
+
         Geocode.fromLatLng(this.state.latitude, this.state.longitude).then(
             (response) => {
               const address = response.results[0].formatted_address;
@@ -64,6 +86,7 @@ class App extends React.Component {
 
       event.preventDefault();
       this.setState({responseFlag: true,
+        distanceNorthPole : _distanceNorthPole,
         moonInfo : _moonInfo,
         countryText : _countryText});
     }
@@ -83,21 +106,19 @@ class App extends React.Component {
         const distance = await moon.getDistanceToEarth();
         const delta = await moon.getAngularDiameter();
         var _moonInfo = 'distance is : ' + distance + "\n"+ 'delta is : ' + delta;
-
         var _longitude;
         var _latitude;
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(this.showPosition) ;
         }
+       var _distanceNorthPole = this.distance(this.state.latitude,this.state.longitude,90,0);
        this.setState({
                 moonInfo : _moonInfo,
-                responseFlag : true });
+                responseFlag : true,
+                distanceNorthPole : _distanceNorthPole
+            });
       }
-    render() {
-        Geocode.setApiKey("AIzaSyAvs-JNQhhyMi38iNEmRe45ehldcnSNce8");
-        Geocode.setLanguage("en");
-        Geocode.setLocationType("ROOFTOP");
-       
+    render() {      
         let inputOptions = [
             {value:1 , label: "GPS"},
             {value:2 , label: "Manual"}
@@ -191,6 +212,9 @@ class App extends React.Component {
                         </p> 
                         <p>
                             Location is  :  {this.state.countryText}  
+                        </p> 
+                        <p>
+                            Distance to NorthPole is  :  {this.state.distanceNorthPole} km.  
                         </p> 
                     </div>
                 : null 
